a81b7c93d10f6f8af9d3d0925f721dc8be381f19
lbeese <lbeese@posteo.net>
2021-12-27 15:42:24 +0100

    trackslot extensions
    trackslot extensions

---
+++ touchdesigner/python/tracker/trackslot_slotutils.py
@@ -0,0 +1,98 @@
+"""
+Extension classes enhance TouchDesigner components with python. An
+extension is accessed via ext.ExtensionClassName from any operator
+within the extended component. If the extension is promoted via its
+Promote Extension parameter, all its attributes with capitalized names
+can be accessed externally, e.g. op('yourComp').PromotedFunction().
+
+Help: search "Extensions" in wiki
+"""
+
+from TDStoreTools import StorageManager
+import TDFunctions as TDF
+
+class Slotutils:
+	"""
+	Slotutils description
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+		self.PageDict = dict([(e.name, e) for e in ownerComp.customPages])
+
+	def ActivatePage(self, pagename):
+		page = self.PageDict[pagename]
+		for p in page.pars:
+			p.enable = True
+
+	def DeactivatePage(self, pagename):
+		page = self.PageDict[pagename]
+		for p in page.pars:
+			p.enable = False
+
+	def Flagupdate(self):
+		active = bool(self.ownerComp.par.Active)
+		performer = bool(self.ownerComp.par.Performer)
+		jail = bool(self.ownerComp.par.Jail)
+		if not active:
+			self.SetInactive()
+		elif performer:
+			self.SetPerformer()
+		elif jail:
+			self.SetJail()
+		else:
+			self.SetParticipant()
+
+	def SetInactive(self):
+		slot = self.ownerComp
+		# slot.name = slot.par.Name
+		# slot.par.Active = False
+		for pagename in ['Position','Geometry','Score','Poll','Jail','Highlight','Scene']:
+			self.DeactivatePage(pagename)
+		pass
+
+	def SetPerformer(self):
+		slot = self.ownerComp
+		# slot.name = slot.par.Name
+		# slot.par.Active = False
+		for pagename in ['Geometry','Score','Poll','Jail','Highlight','Scene']:
+			self.DeactivatePage(pagename)
+		for pagename in ['Position']:
+			self.ActivatePage(pagename)
+		pass
+
+	def SetJail(self):
+		slot = self.ownerComp
+		# slot.name = slot.par.Name
+		# slot.par.Active = False
+		for pagename in ['Geometry','Score','Poll']:
+			self.DeactivatePage(pagename)
+		for pagename in ['Position','Jail','Highlight','Scene']:
+			self.ActivatePage(pagename)
+		pass
+
+	def SetParticipant(self):
+		slot = self.ownerComp
+		# slot.name = slot.par.Name
+		# slot.par.Active = False
+		for pagename in ['Position','Geometry','Score','Poll','Jail','Highlight','Scene']:
+			self.ActivatePage(pagename)
+		pass
+
+	def SetGeoGrab(self):
+		pass
+
+	def UnsetGeoGrab(self):
+		pass
+
+	def Reset(self):
+		pass
+
+	def RoundReset(self):
+		pass
+
+	def RoundStore(self):
+		pass
+
+	def RoundRemove(self):
+		pass
--- touchdesigner/python/tracker/utils.py
+++ touchdesigner/python/tracker/utils.py
@@ -14,29 +14,26 @@ class Utils:
 	def __init__(self, ownerComp):
 		# The component to which this extension is attached
 		self.ownerComp = ownerComp
-
+		
 		# properties
 		self.TrackIds = DependList()
 		self.Assignment = DependDict()
+		self.MaxSlots = op.Settings.par.Maxtracks
 
 		# TODO use reference to ownerComp parameter instead of fixed size to couple this to replicator configuration
-		self.EmptySlots = set(range(1,51))
+		self.EmptySlots = set(range(1, 1 + self.MaxSlots))
 		self.UnassignedIds = list()
 
 	def Reassign(self):
-		self.EmptySlots = set(range(1,51))
+		self.EmptySlots = set(range(1, 1 + self.MaxSlots))
 		ad = op('active_dat')
 		tmp = list(self.Assignment.keys())
-		for i in range(1,51):
-			tslot = op(f'tracker_{i}')
-			tslot.par.Trackid = 0
-			tslot.par.Active = 0
 		for tid in tmp:
 			self.Unassign(tid)
 		for row in ad.rows():
-			tid = row[0].val
-			debug(tid)
-			self.Assign(int(tid))
+			if row[1].val:
+				tid = row[0].val
+				self.Assign(int(tid))
 
 	def ResolveID(self, tid):
 		try:
@@ -47,38 +44,50 @@ class Utils:
 	def Assign(self, tid):
 		slot = self.ResolveID(tid)
 		if not slot:
+			debug(f"trying to assign {tid} to a new slot")
 			if len(self.EmptySlots) <= 0:
 				debug(f"unable to add tracker for id {tid} - no unassigned slots")
 				slot = -1
 			else:
 				slot = self.EmptySlots.pop()
-				self.Assignment[tid] = slot
-				tracker = self.ownerComp.op(f'tracker_{slot}')
-				tracker.par.Trackid = tid
-				self.Setup(slot)
-				debug(f'Assigned {tid} to {slot}')
+				self.Setup(slot, tid)
+				# debug(f'Assigned {tid} to {slot}')
 		return slot
 
 	def Unassign(self, tid):
 		try:
 			slot = self.Assignment.pop(int(tid))
 		except KeyError:
-			# debug(f"Unassign {tid} failed - assignment not found")
 			slot = -1
+			# debug(f"Unassign {tid} failed - assignment not found")
 		else:
-			tracker = self.ownerComp.op(f'tracker_{slot}')
-			tracker.par.Trackid = 0
-			self.Setup(slot)
-			self.EmptySlots.add(slot)
-			debug(f'Unassigned {tid} from {slot}')
+			self.Unset(slot, tid)
+			# debug(f'Unassigned {tid} from {slot}')
 		finally:
 			return slot
 
-	def Setup(self, slot):
+	def Setup(self, slot, tid):
 		slot = int(slot)
-		tracker = self.ownerComp.op(f'tracker_{slot}')
-		tid = int(tracker.par.Trackid)
-		if tid:
-			tracker.par.Active = True
-		else:
-			tracker.par.Active = False
+		tid = int(tid)
+		
+		self.Assignment[tid] = slot
+
+		tracker = self.ownerComp.op(f'slot_{slot}')
+		tracker.name = f'tracker_{tid}'
+		tracker.par.Trackid = tid
+		tracker.par.opshortcut = f'tracker_{tid}'
+		tracker.par.Active = True
+		# tracker.par.play = True
+
+	def Unset(self, slot, tid):
+		slot = int(slot)
+		tid = int(tid)
+		
+		tracker = self.ownerComp.op(f'tracker_{tid}')
+		tracker.name = f'slot_{slot}'
+		tracker.par.Trackid = 0
+		tracker.par.opshortcut = ''
+		tracker.par.Active = False
+		# tracker.par.play = False
+
+		self.EmptySlots.add(slot)
